6045d893a715aedf03c633ae81bd30c90b1ee9a4
Ahmed Osman <ahmedosman36610@gmail.com>
2022-02-25 18:19:50 -0800

    basic file setup of project

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <>
+      <nav>
+        <li>Icon</li>
+        <li>BluTube</li>
+        <input type="text" />
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
